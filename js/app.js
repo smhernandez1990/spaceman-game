d0a8b3a6523272c76e17f3d68492c53a611b1f47
smhernandez1990 <samanthamaria.hernandez90@gmail.com>
2026-01-15 22:08:56 -0500

    updates to html classes and ids, init function, loadlevel function, added event listener and handleclick function

--- js/app.js
+++ js/app.js
@@ -18,9 +18,11 @@ Hint: A subgenre of industrial music coined in the early 1980s in England often 
 
 let currentWord;
 let currentLevel;
-let gameboard;
 let incorrectGuesses;
 let guessedLetters = [];
+let win;
+let lose;
+let gameboard;
 
 // current word, current level, gameboard, number of incorrect guesses, guessed letters array
 
@@ -29,19 +31,22 @@ let guessedLetters = [];
 const spacemanImgDisplayEl = document.querySelector('.spacemanImgDisplay');
 const letterButtonsEls = document.querySelector('.letterButtons');
 const hintDisplayEl = document.querySelector('#hintDisplay');
-const gameboardDisplayEl = document.querySelector('.gameboardDisplay');
+const lvlOneBoard = document.querySelector('.lvlOne');
+const lvlTwoBoard = document.querySelector('.lvlTwo');
+const lvlThreeBoard = document.querySelector('.lvlThree');
 
 // spaceman image display, letter buttons, hint display, level gameboard
 
 // 3. Create an initialization function.
 
 function init() {
-    currentLevel = 5;
-    loadLevel();
+    currentLevel = 1;
+    gameboard = lvlOneBoard;
+    win = false;
+    lose = false;
     incorrectGuesses = 0;
     guessedLetters = [];
-    updateSpacemanImage();
-    render();
+    loadLevel();
 }
 
 init();
@@ -49,30 +54,57 @@ init();
 function loadLevel() {
     if (currentLevel === 1) {
         hintDisplayEl.textContent = 'HINT: Most residents of Olso, Norway would probably agree that this genre of music is Norway\'s biggest cultural export.';
+        lvlTwoBoard.style.display = 'none';
+        lvlThreeBoard.style.display = 'none';
+        currentWord = 'BLACKMETAL';
+        if (currentWord === guessedLetters.join('')){
+            currentLevel = 2;
+            gameboard = lvlTwoBoard
+            alert('Alright Fenriz, we get it you heard A Blaze In The Northern Sky in high school but try this next one out')
+            incorrectGuesses = 0
+            guessedLetters = []
+        };
+
     } else if (currentLevel === 2) {
         hintDisplayEl.textContent = 'HINT: Artists C.C.C.C., Incapacitants, Masonna and Hanatarash are among the notable pioneers of this regional style of experimental music.';
+        lvlOneBoard.style.display = 'none';
+        lvlThreeBoard.style.display = 'none';
+        currentWord = 'JAPANOISE';
+        if (currentWord === guessedLetters.join('')){
+            currentLevel = 3;
+            gameboard = lvlThreeBoard
+            incorrectGuesses = 0
+            guessedLetters = []
+            alert('Okay okay, your local record store had a freak music section. But what about something a little more depraved?')
+        }
+
     } else if (currentLevel === 3) {
         hintDisplayEl.textContent = 'HINT: A subgenre of industrial music coined in the early 1980s in England often characterized by abrasive synthesizers, aggressive vocals and provocative themes.';
-    }
+        lvlOneBoard.style.display = 'none';
+        lvlTwoBoard.style.display = 'none';
+        currentWord = 'POWERELECTRONICS';
+        if (currentWord === guessedLetters.join('')){
+            win = true;
+        };
+    };
 };
 
-function updateSpacemanImage(incorrectGuesses) {
-    if (incorrectGuesses === 5) {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/2EewjBC.jpg';
-    }else if (incorrectGuesses === 4) {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/QDmhY1D.png';
-    }else if (incorrectGuesses === 3) {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/fMXcJfX.png';
-    }else if (incorrectGuesses === 2) {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/uoZC9gL.png';
-    }else if (incorrectGuesses === 1) {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/A27buek.png';
-    }else {
-        spacemanImgDisplayEl.src = 'https://i.imgur.com/SwKfSSw.png';
-    }
-}
+function handleClick(event) {
+    const letterClicked = event.target.textContent;
+    console.log(letterClicked);
+    if (currentWord.includes(letterClicked)) {
+        guessedLetters.push(letterClicked);
+        console.log(guessedLetters);
+        
+    } else {
+        incorrectGuesses++;
+        console.log(incorrectGuesses);
+        
+    };
+};
 
-console.log(updateSpacemanImage);
+
+letterButtonsEls.addEventListener('click', handleClick)
 
 
 // set current level to 1, load first word and hint, reset spaceman image and guessed letters array
@@ -80,19 +112,19 @@ console.log(updateSpacemanImage);
 // 4. Define win/lose conditions.
 
 //IF all letters in the word are guessed correctly AND current level is less than 3
-    // THEN display win alert and move to next level
+// THEN display win alert and move to next level
 //ELSE IF all letters in the word are guessed correctly AND current level is 3
-    // THEN display final win alert 
+// THEN display final win alert
 //ELSE display lose alert
 
 // 5. Define input handling.
 
 //Add event listeners to letter buttons
-    // IF letter guessed correctly
-        // THEN update gameboard display to display letter in correct positions
-    // ELSE
-        // increment incorrect guess count
-        // update spaceman image display
+// IF letter guessed correctly
+// THEN update gameboard display to display letter in correct positions
+// ELSE
+// increment incorrect guess count
+// update spaceman image display
 
 // 6. How will the spaceman image be updated with each incorrect guess?
 
